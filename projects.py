e4dbb2abca0a9ebbb081e25e542977c3058247a7
rrathip <44304650+rrathip@users.noreply.github.com>
2018-10-29 13:38:41 -0700

    Update projects.py

--- projects.py
+++ projects.py
@@ -67,3 +67,84 @@ z = c//2 + c%2
 
 print(x+y+z)
 
+#last digit of integer
+x = int(input())
+print(x%10)
+
+#Tens digits
+x = int(input())
+y = x//10
+print(y%10)
+
+#Sum of digits
+x = int(input())
+a = x%10
+y = x//10
+b = y%10
+c = x//100
+
+print(a+b+c)
+
+#Fractional Part
+from math import floor
+
+a = float(input())
+b = floor(a)
+int(b)
+c = (a - b)
+print(c)
+
+#First Digit after decimal point
+x = float(input())
+b = int(x)
+c = x-b
+d = int(c/0.1)
+print(d)
+
+#Car Route 
+from math import ceil
+a = int(input())
+b = int(input())
+
+c = b/a
+
+x= ceil(c*1)
+
+print(x)
+
+#Digital Clock
+n = int(input())
+h = n//60
+m = n%60
+
+print(h)
+print(m)
+
+#Total Cost
+A = int(input())
+B = int(input())
+C = int(input())
+
+x = A*100
+y = B
+z = x + B
+
+
+print(((z*C)//100), (z*C%100))
+
+
+#Clock Face 1
+h =int(input())
+m =int(input())
+s =int(input())
+
+x = (h*3600)+(m*60)+s
+
+print(x*0.00833333)
+
+#Clock Face 2
+a = float(input())
+
+x = a%30
+
+print(x*12)
